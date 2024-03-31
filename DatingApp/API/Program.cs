ef8f61b02cf7114d4f2e31fbc6fbeacea18e0633
sithembiso <31868918+pro-lover@users.noreply.github.com>
2024-03-31 18:14:46 +0200

    push

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -1,31 +1,3 @@
-using System.Transactions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.logging;
-
-namespace API
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
